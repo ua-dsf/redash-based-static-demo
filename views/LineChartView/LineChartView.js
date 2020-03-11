e7967f61d637c557b78022746def06ae4c40ad45
Alex Bigelow <alex.r.bigelow@gmail.com>
2020-03-11 13:19:43 -0700

    Temporarily pausing infrastructure explorations, because I think the regular API is misbehaving

--- views/LineChartView/LineChartView.js
+++ views/LineChartView/LineChartView.js
@@ -1,26 +1,50 @@
+/* globals d3 */
 import GoldenLayoutView from '../common/GoldenLayoutView.js';
 import SvgViewMixin from '../common/SvgViewMixin.js';
 
 class LineChartView extends SvgViewMixin(GoldenLayoutView) {
   constructor (argObj) {
-    // CORS is restricted to WebDAV, so we use a local csv for testing
-    const csvUrl = window.location.hostname === 'localhost'
-      ? './data/example.csv'
-      : 'https://carrot.atmo.cloud/api/queries/72/results.csv?api_key=q38NtQIaK0Ebw4Zq45oq9liC0Ckk8v0MMItcMnLP';
     argObj.resources = [
       { type: 'less', url: './views/LineChartView/style.less' },
       { type: 'text', url: './views/LineChartView/template.svg' },
-      { type: 'csv', url: csvUrl }
+      { type: 'text', url: './views/LineChartView/controls.html' }
     ];
     super(argObj);
   }
   get title () {
     return 'DE Jobs Over Last Year';
   }
+  async getData () {
+    // CORS is restricted to WebDAV, so we use a local csv for testing
+    const csvUrl = window.location.hostname === 'localhost'
+      ? './data/example.csv'
+      : 'https://carrot.atmo.cloud/api/queries/85/results.csv?api_key=q38NtQIaK0Ebw4Zq45oq9liC0Ckk8v0MMItcMnLP';
+    const response = await window.fetch(csvUrl, {
+      method: 'POST',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      body: { id: 85, parameters: { 'APP_NAME': 'Cufflinks2' } } });
+    if (response.status === 200) {
+      this.data = d3.csvParse(await response.text());
+    } else {
+      console.warn('Error fetching data');
+      console.log(response);
+    }
+    this.render();
+  }
   setup () {
     super.setup();
     // Apply the template
     this.content.html(this.resources[1]);
+    // Insert the controls outside
+    this.d3el.append('div')
+      .classed('controls', true)
+      .html(this.resources[2]);
+    this.d3el.select('.submit.button')
+      .on('click', async () => {
+        this.getData();
+      });
+    this.getData();
   }
   draw () {
     super.draw();
@@ -29,7 +53,7 @@ class LineChartView extends SvgViewMixin(GoldenLayoutView) {
       return;
     }
 
-    console.log(this.resources[2]);
+    console.log(this.data);
   }
 }
 
